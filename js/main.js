eb51fa8d45003b8f640ed06376339be29fc06c3c
Andrew <andrei_efremov@inbox.ru>
2019-09-29 19:48:22 +0400

    create elements, there are some problems whith attributes

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-
+'use strict'
 var getRandomNumber = function (minNumber, maxNumber) {
   return Math.floor(minNumber + Math.random() * (maxNumber - minNumber));
 };
-var accomodation = ["palace", "flat", "house", "bungalo"];
+var accomodation = ['palace', 'flat', 'house', 'bungalo'];
 
-var checkinTimes = ["12:00", "13:00", "14:00"];
+var checkinTimes = ['12:00', '13:00', '14:00'];
 
-var checkoutTimes = ["12:00", "13:00", "14:00"];
+var checkoutTimes = ['12:00', '13:00', '14:00'];
 
-var featuresArray = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-var photosArray = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel4.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel5.jpg"
+var photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel4.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel5.jpg'
 ];
 
 var createIncomeArray = function () {
   var incomeArray = [];
   for(var i = 1; i <=8; i++){
-    var location = {"x" : 600, "y" : 300};
+    var location = {'x' : 600, 'y' : 300};
     var incomeObject = {
-      "author": {
-        "avatar": "img/avatars/user0" + i + ".png"
+      'author': {
+        'avatar': 'img/avatars/user0' + i + '.png'
        },
 
-      "offer": {
-        "title": "заголовок 0" + i,
-        "address": "location.x, location.y}}",
-        "price": 400,
-        "type": accomodation[getRandomNumber(1, 5)],
-        "rooms": getRandomNumber(0, 4),
-        "guests": getRandomNumber(0, 6),
-        "checkin": checkinTimes[getRandomNumber(0, 3)],
-        "checkout": checkoutTimes[getRandomNumber(0, 3)],
-        "features": featuresArray.slice(1, getRandomNumber(1, 5)),
-        "description": "описание",
-        "photos": photosArray.slice(0, getRandomNumber(1, 5))
+      'offer': {
+        'title': 'заголовок 0' + i,
+        'address': 'location.x, location.y}}',
+        'price': 400,
+        'type': accomodation[getRandomNumber(1, 5)],
+        'rooms': getRandomNumber(0, 4),
+        'guests': getRandomNumber(0, 6),
+        'checkin': checkinTimes[getRandomNumber(0, 3)],
+        'checkout': checkoutTimes[getRandomNumber(0, 3)],
+        'features': featuresArray.slice(1, getRandomNumber(1, 5)),
+        'description': 'описание',
+        'photos': photosArray.slice(0, getRandomNumber(1, 5))
       },
 
-      "location": {
-        "x": getRandomNumber(0, 200),
-        "y": getRandomNumber(130, 630),
+      'location': {
+        'x': getRandomNumber(0, 200),
+        'y': getRandomNumber(130, 630),
       }
     }
     incomeArray.push(incomeObject);

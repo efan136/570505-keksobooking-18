7fd14c1084cc684984702784a93342f53d38f304
Andrew <andrei_efremov@inbox.ru>
2019-10-04 04:32:56 +0400

    draw pins

--- js/main.js
+++ js/main.js
@@ -1,90 +1,120 @@
 'use strict';
 
-var accomodation = ['palace', 'flat', 'house', 'bungalo'];
+var map = document.querySelector('.map');
+var mapPins = document.querySelector('.map__pins');
 
-var checkinTimes = ['12:00', '13:00', '14:00'];
-
-var checkoutTimes = ['12:00', '13:00', '14:00'];
-
-var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-var photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+var ACCOMMODATION = ['palace', 'flat', 'house', 'bungalo'];
+var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ACCOMMODATION_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel4.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel5.jpg'
 ];
 
-var accomodationPrice = 400;
-var accomodationDescription = 'описание';
-var offerTitle = 'заголовок 0';
-var adressLocation = {'x': 600, 'y': 350};
+var ACCOMMODATION_PRICE = 400;
+var ACCOMMODATION_DESCRIPTION = 'описание';
+var OFFER_TITLE = 'offer heading 0';
+var ADDRESS_LOCATION = {'x': 600, 'y': 350};
 
 var getRandomNumber = function (minNumber, maxNumber) {
-  return Math.floor(minNumber + Math.random() * (maxNumber - minNumber));
+  return Math.floor(minNumber + Math.random() * (maxNumber + 1 - minNumber));
 };
 
-var getRandomArrayElement = function (arr, minIndex, maxIndex) {
-  return arr[Math.floor(minIndex + Math.random() * (maxIndex - minIndex))];
+var getRandomElementFromArray = function (arr) {
+  return arr[Math.floor(arr.length * Math.random())];
 };
 
 var sliceArrayRandom = function (arr) {
   return arr.slice(1, getRandomNumber(2, arr.length));
 };
 
-var getAccomodationData = function () {
-  var accomodationData = [];
+var getWidthElement = function (element) {
+  return element.getBoundingClientRect().width;
+};
+
+var removeClass = function (element, classElement) {
+  element.classList.remove(classElement);
+};
+
+var getAccommodationParams = function () {
+  var accommodationParams = [];
   for (var i = 1; i <= 8; i++) {
     var incomeObject = {
-      'author': {
+      author: {
         'avatar': 'img/avatars/user0' + i + '.png'
       },
 
-      'offer': {
-        'title': offerTitle + i,
-        'adress': adressLocation,
-        'price': accomodationPrice,
-        'type': getRandomArrayElement(accomodation, 0, 4),
-        'rooms': getRandomNumber(1, 4),
-        'guests': getRandomNumber(1, 6),
-        'checkin': getRandomArrayElement(checkinTimes, 0, 3),
-        'checkout': getRandomArrayElement(checkoutTimes, 0, 3),
-        'features': sliceArrayRandom(featuresArray),
-        'description': accomodationDescription,
-        'photos': sliceArrayRandom(photosArray),
+      offer: {
+        title: OFFER_TITLE + i,
+        address: ADDRESS_LOCATION,
+        price: ACCOMMODATION_PRICE,
+        type: getRandomElementFromArray(ACCOMMODATION),
+        rooms: getRandomNumber(1, 4),
+        guests: getRandomNumber(1, 6),
+        checkin: getRandomElementFromArray(CHECKIN_TIMES),
+        checkout: getRandomElementFromArray(CHECKOUT_TIMES),
+        features: sliceArrayRandom(FEATURES),
+        description: ACCOMMODATION_DESCRIPTION,
+        photos: sliceArrayRandom(ACCOMMODATION_PHOTOS),
       },
 
-      'location': {
-        'x': getRandomNumber(150, 800),
-        'y': getRandomNumber(130, 630),
+      location: {
+        x: getRandomNumber(0, getWidthElement(map)),
+        y: getRandomNumber(130, 630),
       }
     };
-    accomodationData.push(incomeObject);
+    accommodationParams.push(incomeObject);
   }
-  return accomodationData;
+  return accommodationParams;
 };
 
-var allAccomodations = getAccomodationData();
+var allAccommodations = getAccommodationParams();
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var createButton = function (type, className, x, y) {
+  var button = document.createElement('button');
+  button.type = type;
+  button.className = className;
+  button.style.left = x + 'px';
+  button.style.top = y + 'px';
+  return button;
+};
 
-var mapPins = document.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
-for (var i = 0; i <= allAccomodations.length - 1; i++) {
-  var mapPin = document.createElement('button');
-  mapPin.type = 'button';
-  mapPin.className = 'map__pin';
-  mapPin.style.left = allAccomodations[i].location.x + 'px';
-  mapPin.style.top = allAccomodations[i].location.y + 'px';
-  var mapPinImg = document.createElement('img');
-  mapPinImg.src = allAccomodations[i].author.avatar;
-  mapPinImg.alt = allAccomodations[i].offer.title;
-  mapPinImg.width = '40';
-  mapPinImg.height = '40';
+var createImg = function (src, title, width, height) {
+  var img = document.createElement('img');
+  img.src = src;
+  img.alt = title;
+  img.width = String(width);
+  img.height = String(height);
+  return img;
+};
 
-  mapPin.appendChild(mapPinImg);
+removeClass(map, 'map--faded');
 
-  fragment.appendChild(mapPin);
-}
-mapPins.appendChild(fragment);
+var drawPins = function (arr) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i <= arr.length - 1; i++) {
+    var mapPin = createButton(
+        'button',
+        'map__pin',
+        arr[i].location.x - 25, // решил добавить сюда координаты, на которые указывает метка своим острым концом.
+        arr[i].location.y - 70 //  решил добавить сюда координаты, на которые указывает метка своим острым концом.
+    );
+
+    var mapPinImg = createImg(
+        arr[i].author.avatar,
+        arr[i].offer.title,
+        40,
+        40
+    );
+
+    mapPin.appendChild(mapPinImg);
+    fragment.appendChild(mapPin);
+  }
+
+  mapPins.appendChild(fragment);
+};
+
+drawPins(allAccommodations);
